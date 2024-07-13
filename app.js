656fb198ab2132a21c6519af82ca35cb4fde10bd
Jan <148061512+Papssz@users.noreply.github.com>
2024-07-13 15:24:20 +0800

    repo create

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "sysdev_recruitment",
+  });
+
 app.get('/', (req, res) => {
-    res.send('Javascript');
+    const favorite = req.query.favorite;
+
+    const insertQuery = 'INSERT INTO programming_languages (favorite) VALUES (?)';
+    connection.query(insertQuery, [favorite], (err, result) => {
+        if (err) throw err;
+        res.send('Data inserted');
+    });
+
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.get('/programming-languages', (req, res) => {
+    const selectQuery = 'SELECT * FROM programming_languages';
+    connection.query(selectQuery, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
 });
 
-// Command to run: node app.js
+app.listen(port)
+
